63530d3ecb71764b92a19f5a0bc22f6c272990da
Aleksey Dorofeev <aleksej.dorofeev.00@mail.ru>
2023-10-19 23:06:31 +0300

    Добавление форм в проект для получения данных со стороны клиента, реализация трёх этапов обработки данных (аудит, аутентификация и авторизация) для шаблонов с авторизацией и регистрацией, добавление готовых форм в шаблоны, добавлени url-адреса в формах и csrf-токена.

---
+++ store/users/forms.py
@@ -0,0 +1,49 @@
+# Импортируем форму, чтобы сделать её красивой.
+from django import forms
+# Импортируем формы в приложение.
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+# Импортируем модель пользователей.
+from users.models import User
+
+
+class UserLoginForm(AuthenticationForm):
+    # Указываем, что поле с именем пользователя является строкой, добавляем инпут и указываем необходимые атрибуты.
+    # Класс и плейсхолдер берутся из шаблона, указываются их значения.
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Введите имя пользователя'
+    }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Введите пароль'
+    }))
+
+    # Данный класс принимает информацию с какой моделью будет работать форма и с какими именно полями.
+    class Meta:
+        # Указываем, что данная форма или класс будет работать с моделью пользователей.
+        model = User
+        # Указываем, что форма будет работать с полями имя пользователя и пароль.
+        fields = ('username', 'password')
+
+
+class UserRegistrationForm(UserCreationForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Введите имя'
+    }))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Введите фамилию'
+    }))
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Введите имя пользователя'
+    }))
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Введите адрес эл. почты'
+    }))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Введите пароль'
+    }))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Подтвердите пароль'
+    }))
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2')
--- store/users/views.py
+++ store/users/views.py
@@ -1,10 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+# Импортируем приложение auth, чтобы узнать существует ли пользователь.
+from django.contrib import auth
+from django.urls import reverse
+
+from users.models import User
+# Импортируем класс, чтобы присоединить форму к приложению.
+from users.forms import UserLoginForm, UserRegistrationForm
 
 
 # Create your views here.
 def login(request):
-    return render(request, 'users/login.html')
+    # Чтобы проверить какой запрос пришёл необходимо сделать условие, так как данная страница будет работать на два
+    # запроса, предоставление информации пользователю GET и получение информации о пользователе POST.
+    if request.method == 'POST':
+        # Если происходит POST запрос на получение информации о клиенте сервером, то необходимо передавать данные.
+        form = UserLoginForm(data=request.POST)
+        # Необходимо делать проверку на валидность.
+        if form.is_valid():
+            # Если данные проходят валидацию, то необходимо достать данные, которые пришли.
+            username = request.POST['username']
+            password = request.POST['password']
+            # Необходимо достать данного пользователя из базы данных, чтобы понять существует ли он вообще.
+            # Чтобы узнать есть ли данный пользователь передаём его имя и пароль.
+            user = auth.authenticate(username=username, password=password)
+            # Если пользователь есть, то мы его авторизуем, передавая запрос и пользователя.
+            if user:
+                auth.login(request, user)
+                # Перенаправляем пользователя после авторизации на главную страницу.
+                # Если в reverse передать название пути из файла urls.py, то он возвращает адрес.
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        # Если происходит GET запрос, то возвращаем нашу форму.
+        form = UserLoginForm()
+    # Обращаемся через ключ к нашей форме, чтобы она отображалась.
+    context = {'form': form}
+    return render(request, 'users/login.html', context)
 
 
 def registration(request):
-    return render(request, 'users/registration.html')
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            # Если форма валидна, то необходимо сохранить объект в базе данных.
+            form.save()
+            return HttpResponseRedirect(reverse('users:login'))
+    else:
+        form = UserRegistrationForm()
+    context = {'form': form}
+    return render(request, 'users/registration.html', context)
