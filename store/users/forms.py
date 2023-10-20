32b21fd8b3e5dd966d4b927ead4d32c9da8ef8b8
Aleksey Dorofeev <aleksej.dorofeev.00@mail.ru>
2023-10-20 23:00:20 +0300

    Добавление страницы пользователя, добавление контроллера для изменения данных на странице пользователя и загрузки изображения, добавление контроллера на выход из системы, добавление вывода сообщения при валидной форме и при ошибках с соответствующим светом, подключение профиля и выхода из профиля к urls, добавление страницы профиля, добавление вывода информации пользователя в его профиле, добавление возможности перейти в админ-панель, если авторизован супер пользователь или сотрудник и скрытие данной возможности для обычных пользователей, добавление обработки ошибок.

--- store/users/forms.py
+++ store/users/forms.py
@@ -1,7 +1,7 @@
 # Импортируем форму, чтобы сделать её красивой.
 from django import forms
 # Импортируем формы в приложение.
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 # Импортируем модель пользователей.
 from users.models import User
 
@@ -47,3 +47,26 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2')
+
+
+class UserProfileForm(UserChangeForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4'
+    }))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4'
+    }))
+    image = forms.ImageField(widget=forms.FileInput(attrs={
+        'class': 'custom-file-input'
+    }), required=False)
+    # readonly ставим в значение True, чтобы можно было только читать, а не изменять значение.
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4', 'readonly': True
+    }))
+    email = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4', 'readonly': True
+    }))
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'image', 'username', 'email')
--- store/users/views.py
+++ store/users/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render, HttpResponseRedirect
 # Импортируем приложение auth, чтобы узнать существует ли пользователь.
-from django.contrib import auth
+from django.contrib import auth, messages
 from django.urls import reverse
 
 from users.models import User
 # Импортируем класс, чтобы присоединить форму к приложению.
-from users.forms import UserLoginForm, UserRegistrationForm
+from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 
 
 # Create your views here.
+# Контроллер для авторизации.
 def login(request):
     # Чтобы проверить какой запрос пришёл необходимо сделать условие, так как данная страница будет работать на два
     # запроса, предоставление информации пользователю GET и получение информации о пользователе POST.
@@ -37,14 +38,41 @@ def login(request):
     return render(request, 'users/login.html', context)
 
 
+# Контроллер для регистрации.
 def registration(request):
     if request.method == 'POST':
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             # Если форма валидна, то необходимо сохранить объект в базе данных.
             form.save()
+            # Выводим сообщение пользователю об успешной регистрации.
+            messages.success(request, 'Поздравляем! Вы успешно зарегистрировались!')
             return HttpResponseRedirect(reverse('users:login'))
     else:
         form = UserRegistrationForm()
     context = {'form': form}
     return render(request, 'users/registration.html', context)
+
+
+# Контроллер для профиля.
+def profile(request):
+    if request.method == 'POST':
+        # Чтобы можно было изменять данные и сохранять их, необходимо указать не просто data, но и instance.
+        # При помощи переменной files передаётся изменённое изображение.
+        form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:profile'))
+        else:
+            print(form.errors)
+    else:
+        # Данные должны быть как при GET запросе, так и при POST запросе.
+        form = UserProfileForm(instance=request.user)
+    context = {'title': 'Store - Профиль', 'form': form}
+    return render(request, 'users/profile.html', context)
+
+
+# Контроллер для выхода из системы.
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
